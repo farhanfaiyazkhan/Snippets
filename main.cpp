c02818bd642e96168276039f0ac0b73187365521
Farhan Khan <97162698+farhanfaiyazkhan@users.noreply.github.com>
2024-10-20 17:33:55 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve ()
+{
+
+}
+
+int main ()
+{ 
+  int tt = 1;
+  // cin >> tt;
+  while (tt--)
+  {
+    solve();
+  }
+  return 0;
+}
